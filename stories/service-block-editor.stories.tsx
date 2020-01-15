c032d3b50d9261c97ba9709a649f803385fb1faa
Henrik von Hofmeister <hh@cloudscrape.com>
2020-01-15 13:48:01 +0100

    Added storybook to provider

---
+++ stories/service-block-editor.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BlockMetadata, BlockServiceSpec, BlockType, SchemaKind, TargetConfig} from '@blockware/ui-web-types';
+import {BlockTargetProvider} from '@blockware/ui-web-context';
+import ServiceBlockEditorComponent from '../src/web/ServiceBlockEditorComponent';
+
+import '@blockware/ui-web-components/styles/index.less';
+
+const BLOCK_KIND = 'blocks.blockware.com/v1/Service';
+
+const targetConfig:TargetConfig = {
+  kind: 'my-language-target',
+  name: 'My Language Target',
+  blockKinds: [
+    BLOCK_KIND
+  ]
+};
+
+const ServiceBlock:SchemaKind<BlockServiceSpec, BlockMetadata> = {
+  kind: BLOCK_KIND,
+  metadata: {
+    name: 'My block',
+    version: '1.2.3'
+  },
+  spec: {
+    type: BlockType.SERVICE,
+    target: {
+      kind: targetConfig.kind
+    }
+  }
+};
+
+BlockTargetProvider.register(targetConfig);
+
+export default {
+  title: 'Service Block'
+};
+
+export const Editor = () => <ServiceBlockEditorComponent {...ServiceBlock} />;

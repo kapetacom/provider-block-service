4ba26185a60535cd2bb50a879907c465632e52a3
Henrik Hofmeister <henrik@newdawn.dk>
2022-12-28 13:55:47 +0100

    Adjusted build settings to handle multiple versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const Path = require('path');
+const FS = require('fs');
+const YAML = require('yaml');
+
+const blockInfo = YAML.parse(FS.readFileSync(Path.resolve(__dirname, './blockware.yml')).toString());
+const packageJson = require('./package.json');
 
 module.exports = {
-    mode: 'development',
     entry: {
-        'blockware/block-type-service': Path.resolve(__dirname, "./src/web")
+        [`${blockInfo.metadata.name}:${packageJson.version}`]: {
+            import: Path.resolve(__dirname, "./src/web"),
+            filename: `${blockInfo.metadata.name}.js`
+        }
     },
     output: {
         path: Path.join(process.cwd(), 'web'),
         filename: '[name].js',
-        library: `Blockware.blockTypes["[name]"]`,
-        libraryTarget: 'assign'
+        library: {
+            name: `Blockware.blockTypes["[name]"]`,
+            type: 'assign',
+            export: 'default'
+        }
     },
     module: {
         rules: [
